7586c36a04919d7889ee4dc6fe7f94a6e4e99ee1
Your Name <you@example.com>
2018-01-09 18:42:05 -0300

    Mudança nas views

--- resources/views/App/home.blade.php
+++ resources/views/App/home.blade.php
@@ -1,9 +1,9 @@
 <!--Tela inicial da aplicação-->
 @extends('master')
-
 @section('content')
 @include('logged')
-<div class="row">
+<div class="container">
+  <div class="row">
   <div class="col"></div>
   <div class="col-8">
     @if( $arquivos == NULL )
@@ -12,41 +12,42 @@
     </div>
     @else
       @if(count($arquivos)/6 < 1)
+        <div class="container">
         <div class="row">
           @for($i = 0 ; $i < count($arquivos); $i++)
-            <div class="col-sm">
+            <div class="col-lg">
                 <div class="card">
-                  <div class="card-header">
+                  <div class="card-footer">
                       {{$arquivos[$i]->nome}}
                   </div>
-                  <div class="card-body">
-                      <a href="{{ asset($arquivos[$i]->rota) }}"><img style="width:100%;height:100%" class="rounded" src="{{ asset($arquivos[$i]->rota) }}" alt=""></a>
-                  </div>
+                      <a href="{{ asset($arquivos[$i]->rota) }}"><img class="card-img-top img-fluid" src="{{ asset($arquivos[$i]->rota) }}" alt=""></a>
                 </div>
+            </div>
             </div>
           @endfor
         </div>
+      </div>
       @else
         @for($i = 0; $i < count($arquivos)/6; $i++)
+        <div class="container">
         <div class="row">
           @for($j = 0; $j < 6; $j++)
-              <div class="col-sm">
-                  <div class="card" style="width:100%;height:100%">
-                    <div class="card-header">
-                      {{ $arquivos[$i*6+$j]->nome }}
-                    </div>
-                    <div class="card-body">
-                        <a href="{{ asset($arquivos[$i*6+$j]->rota) }}"><img style="width:100%;height:100%;" src="{{ asset($arquivos[$i*6+$j]->rota) }}" alt=""></a>
-                    </div>
+              <div class="col-lg">
+                <div class="card">
+                    <a href="{{ asset($arquivos[$i*6+$j]->rota) }}"><img style="vertical-align:bottom;" class="card-img-bot img-fluid" src="{{ asset($arquivos[$i*6+$j]->rota) }}" alt=""></a>
+                  <div class="card-footer">
+                    {{ $arquivos[$i*6+$j]->nome }}
                   </div>
+                </div>
               </div>
           @endfor
         </div>
+      </div>
         @endfor
       @endif
     @endif
   </div>
   <div class="col"></div>
 </div>
-
+</div>
 @endsection
--- resources/views/logged.blade.php
+++ resources/views/logged.blade.php
@@ -1,20 +1,17 @@
-<div class="row">
-  <div class="col">
-      <nav class="navbar bg-dark navbar-expand-sm navbar-dark">
-          <a href="{{ action('HomeController@index') }}" class="navbar-brand">Olá, mundo!<a>
+<nav class="navbar bg-dark navbar-expand-lg navbar-dark fixed-top">
+  <div class="container">
+    <a href="{{ action('HomeController@index') }}" class="navbar-brand">Olá, mundo!<a>
 
-          <ul class="navbar-nav">
-              <li class="nav-item"> <a class="nav-link" href="{{ route('adicionar') }}"><span class="fa fa-plus"></span></a></li>
-              <li class="nav-item"> <a class="nav-link" href="{{ route('logout') }}">Sair</a></li>
-              <li class="nav-item dropdown"> <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" >Dropdown</a>
-                <div class="dropdown-menu">
-                  <a href="#" class="dropdown-item">Link 1</a>
-                  <a href="#" class="dropdown-item">Link 2</a>
-                  <a href="#" class="dropdown-item">Link 3</a>
-                </div>
-              </li>
-          </ul>
-
-      </nav>
-  </div>
-</div>
+      <ul class="navbar-nav">
+        <li class="nav-item"> <a class="nav-link" href="{{ route('adicionar') }}"><span class="fa fa-plus"></span></a></li>
+        <li class="nav-item"> <a class="nav-link" href="{{ route('logout') }}">Sair</a></li>
+        <li class="nav-item dropdown"> <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" >Dropdown</a>
+          <div class="dropdown-menu">
+            <a href="#" class="dropdown-item">Link 1</a>
+            <a href="#" class="dropdown-item">Link 2</a>
+            <a href="#" class="dropdown-item">Link 3</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
